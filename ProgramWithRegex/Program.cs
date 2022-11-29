43f2fda51069f6ac1ca6f23317a2008ac8046077
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-29 16:28:07 +0530

    [Lalit] Add: Check Pattern to validate Postal Code

--- ProgramWithRegex/Program.cs
+++ ProgramWithRegex/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RegexDemo;
+using System;
+namespace ProgrammingWithRegex
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Programming With Regex Problem Statements");
+            Console.WriteLine("Select Below Mention Options");
+            bool choice = true;
+            while (choice)
+            {
+                Console.WriteLine("1. Check Pattern to validate Postal Code\n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        PostalCode code = new PostalCode();
+                        code.TestPinCode("400088");
+                        break;
+                    default:
+                        choice = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
+        }
+    }
+}
