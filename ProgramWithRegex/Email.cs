c711edea3604f97ce26e2910af8f05df0a7e7bc7
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-12-02 01:13:58 +0530

    [Lalit] Add : Validate Email optional parts

--- ProgramWithRegex/Email.cs
+++ ProgramWithRegex/Email.cs
@@ -12,6 +12,8 @@ namespace ProgramWithRegex
         const string EMAIL_PART_FIRST = "^[a-z]$";
         const string EMAIL_PART_SECOND = "^[a-zA-Z]+[@][a-zA-Z]{2,}$";
         const string EMAIL_PART_THIRD = "^[a-zA-Z]+[@]([a-zA-Z]{2,})+[.]([a-z]{2,})$";
+        const string EMAIL_PART_OPTIONAL = "^[a-zA-Z]+[.+_-]{0,1}[a-z]+[@][a-zA-Z]+[.][a-z]{2,3}([.][a-z]{2}){0,1}$";
+
 
         public void EmailPartFirst()
         {
@@ -37,10 +39,21 @@ namespace ProgramWithRegex
         }
         public void EmailPartThird()
         {
-            Console.WriteLine("Enter your second Email Addrsss part");
+            Console.WriteLine("Enter your all Email Addrsss part");
             string input = Console.ReadLine();
             {
                 if (Regex.IsMatch(input, EMAIL_PART_THIRD))
+                    Console.WriteLine("Email is valid");
+                else
+                    Console.WriteLine("Email does not valid");
+            }
+        }
+        public void EmailPartOptional()
+        {
+            Console.WriteLine("Enter your optional Email Addrsss part");
+            string input = Console.ReadLine();
+            {
+                if (Regex.IsMatch(input, EMAIL_PART_OPTIONAL))
                     Console.WriteLine("Email is valid");
                 else
                     Console.WriteLine("Email does not valid");
--- ProgramWithRegex/Program.cs
+++ ProgramWithRegex/Program.cs
@@ -15,7 +15,7 @@ namespace ProgrammingWithRegex
             while (choice)
             {
                 Console.WriteLine("1. Check Pattern to validate Postal Code\n2. Restrict Postal Code at beggining\n3. Restrict Postal Code at ending\n" +
-                                  "4. Postal Code with Space\n5. Validate Email First Part/n 6. Validate Email First Part\n7. Validate Email all parts\n8. Exit");
+                                  "4. Postal Code with Space\n5. Validate Email First Part/n 6. Validate Email First Part\n7. Validate Email all parts\n8. Validate Email optional parts\n9. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 PostalCode code = new PostalCode();
                 Email email = new Email();
@@ -41,6 +41,9 @@ namespace ProgrammingWithRegex
                         break;
                     case 7:
                         email.EmailPartThird();
+                        break;
+                    case 8:
+                        email.EmailPartOptional();
                         break;
                     default:
                         choice = false;
