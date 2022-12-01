bb93349ad527d6e7969e0ad3238f471f825ee0ef
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-12-02 01:02:59 +0530

    [Lalit] Add: Validate Email second part

--- ProgramWithRegex/Email.cs
+++ ProgramWithRegex/Email.cs
@@ -9,13 +9,25 @@ namespace ProgramWithRegex
 {
     public class Email
     {
-        const string EMAIL_PART_FIRST = "^[a-z]";
+        const string EMAIL_PART_FIRST = "^[a-z]$";
+        const string EMAIL_PART_SECOND = "^[a-zA-Z]+[@][a-zA-Z]{2,}$";
         public void EmailPartFirst()
         {
-            Console.WriteLine("Enter your Email Addrsss");
+            Console.WriteLine("Enter your first Email Addrsss part");
             string input = Console.ReadLine();
             {
                 if (Regex.IsMatch(input, EMAIL_PART_FIRST))
+                    Console.WriteLine("Email is valid");
+                else
+                    Console.WriteLine("Email does not valid");
+            }
+        }
+        public void EmailPartSecond()
+        {
+            Console.WriteLine("Enter your last Email Addrsss part");
+            string input = Console.ReadLine();
+            {
+                if (Regex.IsMatch(input, EMAIL_PART_SECOND))
                     Console.WriteLine("Email is valid");
                 else
                     Console.WriteLine("Email does not valid");
